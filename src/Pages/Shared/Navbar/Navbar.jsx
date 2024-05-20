75fccacaddfd3be4cf24b8a60761d6cf48c3f62b
AnasCoding1986 <anassust.scw@gmail.com>
2024-05-20 23:35:05 +0600

    m-66-problem

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
     const navOptions = <>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/menu">Our menu</Link></li>
-    <li><Link to="/order/salad">Order</Link></li>
-    <li><Link to="/login">Login</Link></li>
+    <li className="ml-5"><Link to="/">Home</Link></li>
+    <li className="ml-5"><Link to="/menu">Our menu</Link></li>
+    <li className="ml-5"><Link to="/order/salad">Order</Link></li>
+    <li className="ml-5"><Link to="/login">Login</Link></li>
+    <li className="ml-5"><Link to="/signup">SignUp</Link></li>
     </>
 
     return (
@@ -17,7 +18,7 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu list-none menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {
                             navOptions
                         }
@@ -25,7 +26,7 @@ const Navbar = () => {
                 </div>
                 <a className="btn btn-ghost text-xl">Bistro Boss</a>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-center list-none hidden lg:flex">
                 {
                     navOptions
                 }
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,10 +1,76 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../Provider/AuthProvider";
+
+
 
 
 const SignUp = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const {createUser} = useContext(AuthContext);
+
+    const onSubmit = (data) => {
+        console.log(data);
+        createUser(data.email,data.password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+    };
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Helmet>
+                <title>Bistro | Sign Up</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">SignUp now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" {...register("name", { required: true })} placeholder="Name" className="input input-bordered" />
+                                {errors.name && <span>This field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
+                                {errors.email && <span>Email field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} placeholder="password" className="input input-bordered" />
+                                {errors.password && <span>Password must have at least 6 charecter but not more than 20</span>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" className="btn btn-primary" value="Submit" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
